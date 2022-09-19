cfdd02f4e3c9d5ec28b40a7427fa5caefb075ac7
aymen-bouzaida <aymen.bouzaida@externe.bnpparibas.com>
2022-09-19 13:13:27 +0200

    Initial Commit

---
+++ aymen-training-plan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aymen-training-plan
+//
+//  Created by aymen bouzaida on 19/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
